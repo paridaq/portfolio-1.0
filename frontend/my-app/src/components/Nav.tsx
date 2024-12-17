daa5224f16c653805ccf282511def37a319dd0f4
Biswajit <paridabiswa2k@gmail.com>
2024-12-17 22:27:14 +0530

    build the setup

---
+++ frontend/my-app/src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <div className="flex justify-center items-center mt-10">
+  <h1 className="text-black font-bold text-4xl ">
+    Biswajit Parida
+  </h1>
+</div>
+
+        
+      
+    </div>
+  )
+}
+
+export default Nav
